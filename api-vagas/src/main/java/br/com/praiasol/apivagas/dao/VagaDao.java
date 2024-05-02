75476e4a20946069fcf68a0ad0de3ccd1b6e7041
Arthur Torres <54257895+NamelessMiojo@users.noreply.github.com>
2024-05-01 21:11:47 -0300

    v0.0.3-update

--- api-vagas/src/main/java/br/com/praiasol/apivagas/dao/VagaDao.java
+++ api-vagas/src/main/java/br/com/praiasol/apivagas/dao/VagaDao.java
@@ -1,10 +1,16 @@
 package br.com.praiasol.apivagas.dao;
 
 import br.com.praiasol.apivagas.model.Vaga;
+import br.com.praiasol.apivagas.to.VagaTO;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
+import org.hibernate.query.TypedParameterValue;
+import org.hibernate.type.StandardBasicTypes;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class VagaDao {
@@ -28,6 +34,23 @@ public class VagaDao {
   public boolean excluirVaga(Vaga vaga) {
     manager.remove(vaga);
     return true;
+  }
+
+  public Vaga buscarVaga(Long id) throws NoResultException{
+    String sql = "SELECT * FROM RHDP_vaga WHERE id_vaga = :id";
+    return (Vaga) manager.createNativeQuery(sql, Vaga.class)
+        .setParameter("id", new TypedParameterValue<>(StandardBasicTypes.LONG, id))
+        .getSingleResult();
+  }
+
+  public List<Vaga> buscarVaga(VagaTO vaga) {
+    String sql = "SELECT * \n\r"
+        + "FROM vaga \r\n"
+        + "WHERE 1 = 1 \r\n"
+        + "AND (id_vaga = :id OR :id IS NULL \r\n"
+        ;
+    return manager.createNativeQuery(sql, Vaga.class)
+        .setParameter("id", new TypedParameterValue<>(StandardBasicTypes.LONG, vaga.getId()))
   }
 
 }
